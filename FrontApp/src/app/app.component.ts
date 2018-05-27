b207d0b771d5482b7cc5c7f7ebb88701a9abdc4e
Xel Prefna <ndembi_x@etna-alternance.net>
2018-05-28 01:57:24 +0200

    :construction: squelette de la page principale

--- FrontApp/src/app/app.component.ts
+++ FrontApp/src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+  src1 = "assets/images/black-p.jpg";
+  src2 = "assets/images/avanger.jpg";
+  src3 = "assets/images/tueurs.jpg";
+  src4 = "assets/images/reine-n.jpg"
+  label1= "WAKANDA FOR LIFE";
+    itemStringsLeft = [
+    'Windstorm',
+    'Bombasto',
+    'Magneta',
+    'Tornado'
+  ];
+ 
+  itemStringsRight = ['Mr. O', 'Tomato'];
 }
